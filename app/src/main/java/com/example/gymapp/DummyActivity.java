9fa002fd1e17b7633f3823bd471d31f8a574665d
amodnija <amodnij123@gmail.com>
2021-05-21 01:30:28 +0530

    login done

--- app/src/main/java/com/example/gymapp/DummyActivity.java
+++ app/src/main/java/com/example/gymapp/DummyActivity.java
@@ -1,14 +1,14 @@
- package com.example.gymapp;
+package com.example.gymapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class DummyActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_dummy);
     }
 }
