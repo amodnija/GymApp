41643ae365eeaccc3bbe6bfe19dff68613f19430
amodnija <amodnij123@gmail.com>
2021-05-21 04:37:11 +0530

    jugaad admin module made

---
+++ app/src/main/java/com/example/gymapp/User.java
@@ -0,0 +1,13 @@
+package com.example.gymapp;
+
+public class User {
+    public String email, role, gymid;
+
+    public User(){}
+
+    public User(String email,String role, String gymid){
+        this.email = email;
+        this.role = role;
+        this.gymid = gymid;
+    }
+}
