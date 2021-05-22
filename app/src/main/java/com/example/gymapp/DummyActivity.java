8ff57d11eda613f45dc92a1178436a7d0e00337d
Siddh <evilpanda789@gmail.com>
2021-05-23 01:28:36 +0530

    safe 2

--- app/src/main/java/com/example/gymapp/DummyActivity.java
+++ app/src/main/java/com/example/gymapp/DummyActivity.java
@@ -1,14 +1,14 @@
 package com.example.gymapp;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
